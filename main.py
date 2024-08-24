29ccc1a7a6b3494c6efef5351b9283e5a3f3621e
Luis Gonzalez <162064187+LuisJG8@users.noreply.github.com>
2024-08-24 16:24:06 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import requests
+from requests.auth import HTTPBasicAuth
+import datetime
 
-APP_ID = "YOUR APP ID"
-API_KEY = "YOUR API KEY"
-full_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+APP_ID = "YOURS"
+API_KEY = "YOURS"
+full_endpoint = "YOURS"
+
+endpoint = "https://api.uat.syndigo.com/api/auth?"
 
 answer = input("Tell me what exercise you did: ")
+
+username = "YOURS"
+password = "YOURS!"
+auth_token = "YOURS"
+
 
 nutri_headers = {
     "x-app-id": APP_ID,
@@ -21,5 +30,43 @@ information = {
 
 
 new_request = requests.post(url=full_endpoint, headers=nutri_headers, json=information)
-new_json = new_request.json()
-print(new_json)
+exercise_info = new_request.json()
+print(exercise_info)
+
+print(exercise_info["exercises"])
+exercise_ws = exercise_info["exercises"][0]["user_input"].title()
+duration_ws = exercise_info["exercises"][0]["duration_min"]
+calories_ws = exercise_info["exercises"][0]["nf_calories"]
+
+thedate = datetime.date.today()
+thetime = datetime.datetime.now()
+
+hour = str(thetime.hour)
+minute = str(thetime.minute)
+second = str(thetime.second)
+total_time = (hour + ":" + minute + ":" + second)
+
+
+sheety_url = "YOURS"
+
+parameters = {
+                "workout": {
+                    "name": "YOURS",
+                    "email": "YOURS",
+                    "time": total_time,
+                    "exercise": exercise_ws,
+                    "duration": duration_ws,
+                    "calories": duration_ws,
+                    "date": str(thedate),
+    }
+}
+
+headers = {
+    "username": username,
+    "password": password
+}
+
+
+basic = HTTPBasicAuth(username, password)
+requestsPost = requests.post(url=sheety_url, json=parameters, headers=headers, auth=basic)
+print(requestsPost.json())
